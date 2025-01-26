084bae5cbbe22373f5f5dc7065012cd1cf99e499
gentille <tuyisengegentille80@gmail.com>
2025-01-26 19:56:02 +0200

    to add by using function

---
+++ create.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// Data to be written to the file
+const data = 'This is a new file created using Node.js!';
+
+fs.writeFile('kevin.txt', data, (err) => {
+  if (err) {
+    console.log('Error creating the file:', err);
+  } else {
+    console.log('File created and data written successfully!');
+  }
+});
--- example.js
+++ example.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // Data to be written to the file
 const data = 'This is a new file created using Node.js!';
 
-fs.writeFile('example.txt', data, (err) => {
+fs.writeFile('kade.txt', data, (err) => {
   if (err) {
     console.log('Error creating the file:', err);
   } else {

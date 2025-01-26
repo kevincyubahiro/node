53637091d7c2ca0fe3f5dbf52b58a2381df4fbe2
gentille <tuyisengegentille80@gmail.com>
2025-01-26 19:07:55 +0200

    to add by using function

--- read.js
+++ read.js
@@ -1,5 +1,9 @@
 const http=require('http');
 const fs =require('fs');
 http.createServer((req,res)=>{
-    fs.read('table.html',function(error))
+    fs.read('table.html',function(error){
+        res.writeHead(200,{
+            'cont'
+        })
+    })
 })

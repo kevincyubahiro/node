e312f62173221b3a32b5670747a137d864e4bc53
gentille <tuyisengegentille80@gmail.com>
2025-01-26 19:49:55 +0200

    to add by using function

---
+++ fs.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+fs.readFile('kevin.txt',result=>{
+    if(result){
+        console.log('This is gikonko technical secondary school ')
+    }
+else{
+    console.log('no')
+}
+});
+
+
+
--- read.js
+++ read.js
@@ -1,13 +1,14 @@
 const http=require('http');
 const fs =require('fs');
-http.createServer((req,res)=>{
-    fs.read('table.html',function(error, data){
-        res.writeHead(200,{
+http.createServer(function(req,res){
+    fs.readFile('table.html',function(err, data){
+        res.writeHead(200,
+            {
             'content-Type':'text/html'
         });
         res.write(data);
         res.end();
     })
-}).listen(3000,()=>{
-    console.log('http://localhost:3000')
+}).listen(8080,()=>{
+    console.log('our server is start on http://localhost:8080')
 })
